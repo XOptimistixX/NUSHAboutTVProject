62698290397d2424e445828aa0a7f35cda62baa6
Jeff Sieu <jeffsy00@gmail.com>
2017-08-08 22:32:52 +0800

    Made some improvements

--- app/src/main/java/sg/edu/nushigh/schooltvprojectabout/MainActivity.java
+++ app/src/main/java/sg/edu/nushigh/schooltvprojectabout/MainActivity.java
@@ -1,16 +1,22 @@
 package sg.edu.nushigh.schooltvprojectabout;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int wayneClicks = 0;
+    private static final int REQUIRED_CLICKS = 2, MILLIS_BEFORE_RESET = 2000;
+    private CountDownTimer timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,23 +27,23 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView mDeveloperRecyclerView = (RecyclerView) findViewById(R.id.developer_recycler_view);
 
         Person ambrose =    new Person("Ambrose Chua", "Year 5, 2017", getDrawable(R.drawable.ambrose));
-        Person andrew =     new Person("Andrew Yapp", "Year 4, 2017", getDrawable(R.drawable.timeline_circle));
-        Person danielLau =  new Person("Daniel Lau", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person danielLim =  new Person("Daniel Lim Wee Soong", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person jeffSieu =   new Person("Jeff Sieu", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person jeffrey =    new Person("Jeffrey Lee", "Year 4, 2017", getDrawable(R.drawable.timeline_circle));
-        Person jingQuan =   new Person("Chong Jing Quan", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person kokRui =     new Person("Wong Kok Rui", "Year 4, 2017", getDrawable(R.drawable.timeline_circle));
-        Person shiHern =    new Person("Lim Shi Hern", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person hanpu =      new Person("Liu Hanpu", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person kaiJun =     new Person("Tay Kai Jun", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
-        Person ngocLinh =   new Person("Cao Ngoc Linh", "Year 4, 2017", getDrawable(R.drawable.timeline_circle));
-        Person shane =      new Person("Shane Ong", "Year 4, 2017", getDrawable(R.drawable.timeline_circle));
-        Person owen =       new Person("Owen Leong", "Year 5, 2017", getDrawable(R.drawable.timeline_circle));
+        Person andrew =     new Person("Andrew Yapp", "Year 4, 2017", getDrawable(R.drawable.andrew));
+        Person danielLau =  new Person("Daniel Lau", "Year 5, 2017", getDrawable(R.drawable.daniellau));
+        Person danielLim =  new Person("Daniel Lim Wee Soong", "Year 5, 2017", getDrawable(R.drawable.daniellim));
+        Person jeffSieu =   new Person("Jeff Sieu", "Year 5, 2017", getDrawable(R.drawable.jeffsieu));
+        Person jeffrey =    new Person("Jeffrey Lee", "Year 4, 2017", getDrawable(R.drawable.jeffrey));
+        Person jingQuan =   new Person("Chong Jing Quan", "Year 5, 2017", getDrawable(R.drawable.jingquan));
+        Person kokRui =     new Person("Wong Kok Rui", "Year 4, 2017", getDrawable(R.drawable.kokrui));
+        Person shiHern =    new Person("Lim Shi Hern", "Year 5, 2017", getDrawable(R.drawable.shihern));
+        Person hanpu =      new Person("Liu Hanpu", "Year 5, 2017", getDrawable(R.drawable.hanpu));
+        Person kaiJun =     new Person("Tay Kai Jun", "Year 5, 2017", getDrawable(R.drawable.kaijun));
+        Person ngocLinh =   new Person("Cao Ngoc Linh", "Year 4, 2017", getDrawable(R.drawable.ngoclinh));
+        Person shane =      new Person("Shane Ong", "Year 4, 2017", getDrawable(R.drawable.shane));
+        Person owen =       new Person("Owen Leong", "Year 5, 2017", getDrawable(R.drawable.owen));
         Person zayne =      new Person("Zayne Siew", "Year 4, 2017", getDrawable(R.drawable.zayne));
-        Person ziXuan =     new Person("Ng Zi Xuan", "Year 6, 2017", getDrawable(R.drawable.timeline_circle));
-        Person wayne =      new Person("Wayne Tee", "Year 5, 2016", getDrawable(R.drawable.wayne));
-        Person jingXuan =   new Person("Tay Jing Xuan", "Year 5, 2016", getDrawable(R.drawable.timeline_circle));
+        Person ziXuan =     new Person("Ng Zi Xuan", "Year 6, 2017", getDrawable(R.drawable.zixuan));
+        final Person wayne =      new Person("Wayne Tee", "Year 5, 2016", getDrawable(R.drawable.wayne2));
+        Person jingXuan =   new Person("Tay Jing Xuan", "Year 5, 2016", getDrawable(R.drawable.jingxuan));
 
 
 
@@ -100,5 +106,40 @@ public class MainActivity extends AppCompatActivity {
 
         mDeveloperRecyclerView.setAdapter(new AppAdapter(this, apps));
         mDeveloperRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL));
+
+
+        View wayneFace = findViewById(R.id.face);
+        wayneClicks = 0;
+        timer = new CountDownTimer(MILLIS_BEFORE_RESET, MILLIS_BEFORE_RESET) {
+
+            public void onTick(long millisUntilFinished) {
+            }
+
+            public void onFinish() {
+                if (wayneClicks == REQUIRED_CLICKS) {
+                    Toast.makeText(getApplicationContext(), "Launching admin app", Toast.LENGTH_SHORT).show();
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage("sg.edu.nushigh.admin");
+                    if (launchIntent != null) {
+                        startActivity(launchIntent);
+                    }
+                }
+                wayneClicks = 0;
+            }
+        }.start();
+        wayneFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timer.cancel();
+                timer.start();
+                wayneClicks++;
+            }
+        });
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wayneClicks = 0;
     }
 }
